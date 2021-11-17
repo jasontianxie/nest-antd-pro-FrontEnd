9f38328595dcacf2060c46c37edebb36a5ae7c84
jasontianxie <1986tianxie@sina.com>
2021-11-17 23:59:08 +0800

    feat: 添加了一个默认值，用于测试

--- src/pages/NewArticle/index.tsx
+++ src/pages/NewArticle/index.tsx
@@ -6,6 +6,10 @@ import hljs from 'highlight.js';
 import styles from './index.less';
 import 'highlight.js/styles/atom-one-dark.css';
 
+const text = window.atob(
+  'IyMgY2xlYXIgY29va2llCgpgYGBqcwpmdW5jdGlvbiBjbGVhckFsbENvb2tpZSgpIHsKICB2YXIga2V5cyA9IGRvY3VtZW50LmNvb2tpZS5tYXRjaCgvW14gPTtdKyg/PVw9KS9nKTsKICBpZiAoa2V5cykgewogICAgZm9yICh2YXIgaSA9IGtleXMubGVuZ3RoOyBpLS07ICkKICAgICAgZG9jdW1lbnQuY29va2llID0ga2V5c1tpXSArICc9MDtleHBpcmVzPScgKyBuZXcgRGF0ZSgwKS50b1VUQ1N0cmluZygpOwogIH0KfQpgYGAKCiMjIHdyaXRlIGNvb2tpZQoKYGBganMKZnVuY3Rpb24gc2V0Q29va2llKGNfbmFtZSwgdmFsdWUsIGV4cGlyZWRheXMpIHsKICB2YXIgZXhkYXRlID0gbmV3IERhdGUoKTsKICBleGRhdGUuc2V0RGF0ZShleGRhdGUuZ2V0RGF0ZSgpICsgZXhwaXJlZGF5cyk7CiAgZG9jdW1lbnQuY29va2llID0KICAgIGNfbmFtZSArICc9JyArIGVzY2FwZSh2YWx1ZSkgKyAoZXhwaXJlZGF5cyA9PSBudWxsID8gJycgOiAnO2V4cGlyZXM9JyArIGV4ZGF0ZS50b0dNVFN0cmluZygpKTsKfQpgYGAKCiMjIHJlYWQgY29va2llcwoKYGBganMKLy9yZWFkIGNvb2tpZXMKZnVuY3Rpb24gZ2V0Q29va2llKG5hbWUpIHsKICB2YXIgYXJyLAogICAgcmVnID0gbmV3IFJlZ0V4cCgnKF58ICknICsgbmFtZSArICc9KFteO10qKSg7fCQpJyk7CgogIGlmICgoYXJyID0gZG9jdW1lbnQuY29va2llLm1hdGNoKHJlZykpKSByZXR1cm4gYXJyWzJdOwogIGVsc2UgcmV0dXJuIG51bGw7Cn0KYGBgCgotLSBhdXRob3I6amFzb250aWFueGllCgpsaW5rczpbaHR0cHM6Ly93d3cuYmFpZHUuY29tXShodHRwczovL3d3dy5iYWlkdS5jb20pCgpzb3VyY2U6W2JhaWR1XShodHRwczovL3d3dy5iYWlkdS5jb20pCgoKIVtpbWddKGh0dHBzOi8vd3d3LmJhaWR1LmNvbS9pbWcvZmxleGlibGUvbG9nby9wYy9yZXN1bHQucG5nKQ==',
+);
+
 marked.setOptions({
   renderer: new marked.Renderer(),
   highlight: function (code, lang) {
@@ -23,7 +27,7 @@ marked.setOptions({
 });
 
 const NewArticle: React.FC = () => {
-  const [markdownText, setMarkdownText] = useState('');
+  const [markdownText, setMarkdownText] = useState(text);
   return (
     <PageContainer>
       <Card>
@@ -36,8 +40,9 @@ const NewArticle: React.FC = () => {
               onChange={(e: any) => setMarkdownText(e.target.value)}
             ></div> */}
             <textarea
+              value={markdownText}
               className={styles.textarea}
-              onChange={(e: any) => {
+              onChange={(e) => {
                 setMarkdownText(e.target.value);
               }}
             />
